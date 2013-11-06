83b1db9d2f0517b0bb77fae90facbfb0b4c189e6
daowangli@gmail.com <daowangli@gmail.com>
2013-11-06 22:40:44 +0800

    #4

---
+++ src/main/java/org/hibernarm/management/control/action/HibernarmControlAction.java
@@ -0,0 +1,7 @@
+package org.hibernarm.management.control.action;
+
+public class HibernarmControlAction {
+	public String execute() {
+		return "success";
+	}
+}
---
+++ src/main/java/org/hibernarm/management/control/action/HomeAction.java
@@ -0,0 +1,7 @@
+package org.hibernarm.management.control.action;
+
+public class HomeAction {
+	public String execute() {
+		return "success";
+	}
+}
