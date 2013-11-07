eeea04de9bddfbaf61d38b067fe948b73cae3f91
daowangli@gmail.com <daowangli@gmail.com>
2013-11-07 15:52:05 +0800

    #5 Add webservice client to control hibernarm service

--- src/main/java/org/hibernarm/management/control/action/HibernarmControlAction.java
+++ src/main/java/org/hibernarm/management/control/action/HibernarmControlAction.java
@@ -1,7 +1,16 @@
 package org.hibernarm.management.control.action;
 
+import org.hibernarm.service.AQLExecute;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class HibernarmControlAction {
 	public String execute() {
+	    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml", HibernarmControlAction.class);		
+	    AQLExecute client = (AQLExecute)context.getBean("wsclient");
+	    client.stop();
+	    client.reconfigure();
+	    client.start();
 		return "success";
 	}
 }
