d0963fffac6f6b45dde5ccf7ff96de18441e246a
daowangli@gmail.com <daowangli@gmail.com>
2013-11-13 23:30:05 +0800

    #20 Archetype and arm id extraction

--- src/main/java/org/hibernarm/management/archetype/action/FileUploadAction.java
+++ src/main/java/org/hibernarm/management/archetype/action/FileUploadAction.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
 import org.apache.log4j.Logger;
 import org.hibernarm.management.control.action.HibernarmControl;
 import org.hibernarm.management.control.action.HibernarmControlAction;
@@ -17,8 +23,14 @@ import org.hibernarm.management.model.ArchetypeBean;
 import org.hibernarm.management.util.FileUtil;
 import org.hibernarm.management.util.HibernateUtil;
 import org.hibernarm.service.AQLExecute;
+import org.openehr.am.archetype.Archetype;
+import org.openehr.rm.support.identification.ArchetypeID;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import se.acode.openehr.parser.ADLParser;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -61,13 +73,23 @@ public class FileUploadAction extends ActionSupport {
 			throws Exception {
 		List<ArchetypeBean> list = new ArrayList<ArchetypeBean>();
 		for (int i = 0; i < upload.length; i++) {
-			if (uploadFileName[i].endsWith("adl")) {
-				ArchetypeBean archetypeBean = new ArchetypeBean();
-				archetypeBean.setModifyTime(modifyTime);
-				archetypeBean.setContent(FileUtil.extractContent(upload[i]));
-				archetypeBean.setName(uploadFileName[i].substring(0,
-						uploadFileName[i].lastIndexOf(".")));
-				list.add(archetypeBean);
+			if (FileUtil.getFileType(upload[i]).compareToIgnoreCase("xml") != 0) {
+				String content = FileUtil.extractContent(upload[i]);
+				ADLParser parser = new ADLParser(content);
+				Archetype archetype = null;
+				try {
+					archetype = parser.parse();
+				} catch (Exception e) {
+				}
+
+				ArchetypeID archetypeId = archetype.getArchetypeId();
+				if (!archetypeId.toString().isEmpty()) {
+					ArchetypeBean archetypeBean = new ArchetypeBean();
+					archetypeBean.setModifyTime(modifyTime);
+					archetypeBean.setContent(content);
+					archetypeBean.setName(archetypeId.toString());
+					list.add(archetypeBean);
+				}
 			}
 		}
 		return list;
@@ -78,13 +100,25 @@ public class FileUploadAction extends ActionSupport {
 			throws Exception {
 		List<ARMBean> list = new ArrayList<ARMBean>();
 		for (int i = 0; i < uploadFileName.length; i++) {
-			if (uploadFileName[i].endsWith("arm")) {
-				ARMBean armBean = new ARMBean();
-				armBean.setContent(FileUtil.extractContent(upload[i]));
-				armBean.setModifyTime(modifyTime);
-				armBean.setName(uploadFileName[i].substring(0,
-						uploadFileName[i].lastIndexOf(".")));
-				list.add(armBean);
+			if (FileUtil.getFileType(upload[i]).compareToIgnoreCase("xml") == 0) {
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+						.newInstance();
+				DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dbBuilder.parse(upload[i]);
+				XPath xpath = XPathFactory.newInstance().newXPath();
+				Node classNode = (Node) xpath.evaluate(
+						"/hibernate-mapping/class", doc, XPathConstants.NODE);
+				NamedNodeMap classNodeAttributes = classNode.getAttributes();
+				Node classNodeNameNode = classNodeAttributes
+						.getNamedItem("name");
+				String archetypeId = classNodeNameNode.getNodeValue();
+				if (!archetypeId.isEmpty()) {
+					ARMBean armBean = new ARMBean();
+					armBean.setContent(FileUtil.extractContent(upload[i]));
+					armBean.setModifyTime(modifyTime);
+					armBean.setName(archetypeId);
+					list.add(armBean);
+				}
 			}
 		}
 		return list;
@@ -105,8 +139,8 @@ public class FileUploadAction extends ActionSupport {
 			for (ArchetypeBean archetypeBean : listArchetypeBeans) {
 				archetypeBeanDao.saveOrUpdate(archetypeBean);
 			}
-			
-			//validateHibernarm();
+
+			// validateHibernarm();
 		} catch (Exception e) {
 			result = "fail";
 		} finally {
@@ -114,18 +148,19 @@ public class FileUploadAction extends ActionSupport {
 		}
 		return result;
 	}
-	
+
 	protected void validateHibernarm() {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml", HibernarmControlAction.class);
 		AQLExecute client = (AQLExecute) context.getBean("wsclientvalidation");
-		
+
 		HibernarmControl control = new HibernarmControl();
 		control.execute(client);
 	}
 
-	private void saveAction(List<ARMBean>listArmBeans,List<ArchetypeBean>listArchetypeBeans) {
- 		
+	private void saveAction(List<ARMBean> listArmBeans,
+			List<ArchetypeBean> listArchetypeBeans) {
+
 		for (ARMBean armBean : listArmBeans) {
 			armBeanDao.saveOrUpdate(armBean);
 		}
@@ -133,6 +168,5 @@ public class FileUploadAction extends ActionSupport {
 			archetypeBeanDao.saveOrUpdate(archetypeBean);
 		}
 	}
-
 
 }
