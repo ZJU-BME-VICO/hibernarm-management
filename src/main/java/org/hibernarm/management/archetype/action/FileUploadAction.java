b5cbf2c3e769e16839f869c897c49f8c25115a5f
daowangli@gmail.com <daowangli@gmail.com>
2013-11-14 11:30:01 +0800

    #20 Archetype and arm id extraction

--- src/main/java/org/hibernarm/management/archetype/action/FileUploadAction.java
+++ src/main/java/org/hibernarm/management/archetype/action/FileUploadAction.java
@@ -5,12 +5,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
-
 import org.apache.log4j.Logger;
 import org.hibernarm.management.control.action.HibernarmControl;
 import org.hibernarm.management.control.action.HibernarmControlAction;
@@ -21,20 +15,13 @@ import org.hibernarm.management.dao.virtual.ArchetypeBeanDao;
 import org.hibernarm.management.model.ARMBean;
 import org.hibernarm.management.model.ArchetypeBean;
 import org.hibernarm.management.model.CommitSequence;
+import org.hibernarm.management.util.ARMUtil;
+import org.hibernarm.management.util.ArchetypeUtil;
 import org.hibernarm.management.util.FileUtil;
 import org.hibernarm.management.util.HibernateUtil;
 import org.hibernarm.service.AQLExecute;
-import org.openehr.am.archetype.Archetype;
-import org.openehr.am.serialize.ADLSerializer;
-import org.openehr.rm.support.identification.ArchetypeID;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-
-import se.acode.openehr.parser.ADLParser;
-
 import com.opensymphony.xwork2.ActionSupport;
 
 public class FileUploadAction extends ActionSupport {
@@ -77,22 +64,14 @@ public class FileUploadAction extends ActionSupport {
 		List<ArchetypeBean> list = new ArrayList<ArchetypeBean>();
 		for (int i = 0; i < upload.length; i++) {
 			if (FileUtil.getFileType(upload[i]).compareToIgnoreCase("xml") != 0) {
-				ADLParser parser = new ADLParser(upload[i]);
-				Archetype archetype = null;
-				try {
-					archetype = parser.parse();
-				} catch (Exception e) {
-				}
-				
-				ADLSerializer adlSerilizer = new ADLSerializer();
-				String content = adlSerilizer.output(archetype);
-
-				ArchetypeID archetypeId = archetype.getArchetypeId();
-				if (!archetypeId.toString().isEmpty() && !content.isEmpty()) {
+				ArchetypeUtil archetypeUtil = new ArchetypeUtil(upload[i]);
+				String archetypeId = archetypeUtil.getArchetypeId();
+				String archetypeContent = archetypeUtil.getArchetypeContent();
+				if (!archetypeId.isEmpty() && !archetypeContent.isEmpty()) {
 					ArchetypeBean archetypeBean = new ArchetypeBean();
 					archetypeBean.setModifyTime(modifyTime);
-					archetypeBean.setContent(content);
-					archetypeBean.setName(archetypeId.toString());
+					archetypeBean.setContent(archetypeContent);
+					archetypeBean.setName(archetypeId);
 					list.add(archetypeBean);
 				}
 			}
@@ -106,20 +85,12 @@ public class FileUploadAction extends ActionSupport {
 		List<ARMBean> list = new ArrayList<ARMBean>();
 		for (int i = 0; i < uploadFileName.length; i++) {
 			if (FileUtil.getFileType(upload[i]).compareToIgnoreCase("xml") == 0) {
-				DocumentBuilderFactory dbFactory = DocumentBuilderFactory
-						.newInstance();
-				DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
-				Document doc = dbBuilder.parse(upload[i]);
-				XPath xpath = XPathFactory.newInstance().newXPath();
-				Node classNode = (Node) xpath.evaluate(
-						"/hibernate-mapping/class", doc, XPathConstants.NODE);
-				NamedNodeMap classNodeAttributes = classNode.getAttributes();
-				Node classNodeNameNode = classNodeAttributes
-						.getNamedItem("name");
-				String archetypeId = classNodeNameNode.getNodeValue();
-				if (!archetypeId.isEmpty()) {
+				ARMUtil armUtil = new ARMUtil(upload[i]);				
+				String archetypeId = armUtil.getArchetypeId();
+				String armContent = armUtil.getARMContent();
+				if (!archetypeId.isEmpty() && !armContent.isEmpty()) {
 					ARMBean armBean = new ARMBean();
-					armBean.setContent(FileUtil.extractContent(upload[i]));
+					armBean.setContent(armContent);
 					armBean.setModifyTime(modifyTime);
 					armBean.setName(archetypeId);
 					list.add(armBean);
---
+++ src/main/java/org/hibernarm/management/util/ArchetypeUtil.java
@@ -0,0 +1,38 @@
+package org.hibernarm.management.util;
+
+import java.io.File;
+import java.io.IOException;
+import org.openehr.am.archetype.Archetype;
+import org.openehr.am.serialize.ADLSerializer;
+import org.openehr.rm.support.identification.ArchetypeID;
+
+import se.acode.openehr.parser.ADLParser;
+
+public class ArchetypeUtil {
+	private Archetype archetype = null;
+	private String archetypeId = "";
+	private String archetypeContent = "";
+
+	public ArchetypeUtil(File archetypeFile) {
+		try {
+			ADLParser parser = new ADLParser(archetypeFile);
+			archetype = parser.parse();
+			ArchetypeID archetypeIdNode = archetype.getArchetypeId();
+			archetypeId = archetypeIdNode.toString();	
+			ADLSerializer adlSerilizer = new ADLSerializer();
+			archetypeContent = adlSerilizer.output(archetype);
+		} catch (Exception e) {
+			archetype = null;
+			archetypeId = "";
+			archetypeContent = "";
+		}
+	}
+
+	public String getArchetypeId() {
+		return archetypeId;
+	}
+	
+	public String getArchetypeContent() {	
+		return archetypeContent;
+	}
+}
